d2deaa5f500499f323cbf70a2bc41e88895b4103
Reinhard Nägele <reinhard.naegele@mgm-tp.com>
2013-04-17 11:18:52 +0200

    MQA-463: Add missing license header

--- jfunk-core/src/main/java/com/mgmtp/jfunk/core/util/CaptureScreenOnError.java
+++ jfunk-core/src/main/java/com/mgmtp/jfunk/core/util/CaptureScreenOnError.java
@@ -1,3 +1,9 @@
+/*
+ *  Copyright (C) 2013 mgm technology partners GmbH, Munich.
+ *
+ *  See the LICENSE file distributed with this work for additional
+ *  information regarding copyright ownership and intellectual property rights.
+ */
 package com.mgmtp.jfunk.core.util;
 
 import java.lang.annotation.ElementType;
