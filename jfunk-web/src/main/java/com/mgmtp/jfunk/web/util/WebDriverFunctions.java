c5a32c2088d73bb6870293f712346d4c4ce4e1f6
Reinhard Nägele <reinhard.naegele@mgm-tp.com>
2013-05-15 11:27:48 +0200

    Fix Javadoc import

--- jfunk-web/src/main/java/com/mgmtp/jfunk/web/util/WebDriverFunctions.java
+++ jfunk-web/src/main/java/com/mgmtp/jfunk/web/util/WebDriverFunctions.java
@@ -14,6 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
