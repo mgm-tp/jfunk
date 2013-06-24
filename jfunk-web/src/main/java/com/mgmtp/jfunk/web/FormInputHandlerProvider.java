96a5e3ed9d31f14e7bca259ebaabf98c9a66e69f
Reinhard Nägele <reinhard.naegele@mgm-tp.com>
2013-06-24 15:25:04 +0200

    Add missing license headers

--- jfunk-web/src/main/java/com/mgmtp/jfunk/web/FormInputHandlerProvider.java
+++ jfunk-web/src/main/java/com/mgmtp/jfunk/web/FormInputHandlerProvider.java
@@ -1,3 +1,9 @@
+/*
+ *  Copyright (C) 2013 mgm technology partners GmbH, Munich.
+ *
+ *  See the LICENSE file distributed with this work for additional
+ *  information regarding copyright ownership and intellectual property rights.
+ */
 package com.mgmtp.jfunk.web;
 
 import static org.apache.commons.lang3.StringUtils.trimToNull;
