196f4ac85000261778a23311763365ed22916b53
Reinhard Nägele <reinhard.naegele@mgm-tp.com>
2013-11-15 16:59:17 +0100

    Fix ArchiveDataSourceTEst (cherry picked from commit bdcbd210a00bb0ea5dbb9875d3e2630b6d8a4d45)

--- jfunk-data/src/test/java/com/mgmtp/jfunk/data/source/ArchiveDataSourceTest.java
+++ jfunk-data/src/test/java/com/mgmtp/jfunk/data/source/ArchiveDataSourceTest.java
@@ -50,39 +50,16 @@ public class ArchiveDataSourceTest {
 
 	@Test(dataProvider = "dataSources")
 	public void testNextDataSet(final DataSource ds) {
-		DataSet data = ds.getNextDataSet("login");
-		Assert.assertEquals(data.getValue("kurzname"), "dNBaHßA-");
-		Assert.assertEquals(data.getValue("wieselAccountId"), "1000258699");
-
-		data = ds.getNextDataSet("lstb");
-		Assert.assertEquals(data.getValue("freierTextBeanAktuell.name7"), "çØ«!Þáüf£ÍC³_tVÌiÑ3³IÏ©µò2ÔF#");
-		Assert.assertEquals(data.getValue("zeitraumjahr"), "2008");
+		DataSet data = ds.getNextDataSet("google");
+		Assert.assertEquals(data.getValue("countryId"), ":2n");
+		Assert.assertEquals(data.getValue("languageId"), ":8");
+		Assert.assertEquals(data.getValue("searchTerm"), "red");
 	}
 
 	@Test(dataProvider = "dataSources")
 	public void testHasMoreData(final DataSource ds) {
-		Assert.assertTrue(ds.hasMoreData("anteilseignerprofil"));
-		Assert.assertTrue(ds.hasMoreData("antragstk"));
-		Assert.assertTrue(ds.hasMoreData("arbeitnehmerprofil"));
-		Assert.assertTrue(ds.hasMoreData("benutzereinstellungen"));
-		Assert.assertTrue(ds.hasMoreData("euzi"));
-		Assert.assertTrue(ds.hasMoreData("euziprofil"));
-		Assert.assertTrue(ds.hasMoreData("finanzamt"));
-		Assert.assertTrue(ds.hasMoreData("fsak"));
-		Assert.assertTrue(ds.hasMoreData("kapesta"));
-		Assert.assertTrue(ds.hasMoreData("kennnummerprofil"));
-		Assert.assertTrue(ds.hasMoreData("kstr"));
-		Assert.assertTrue(ds.hasMoreData("login"));
-		Assert.assertTrue(ds.hasMoreData("lsta"));
-		Assert.assertTrue(ds.hasMoreData("lstb"));
-		Assert.assertTrue(ds.hasMoreData("sadvf"));
-		Assert.assertTrue(ds.hasMoreData("steuerkontoabfrage"));
-		Assert.assertTrue(ds.hasMoreData("steuernummerprofil"));
-		Assert.assertTrue(ds.hasMoreData("ustdv"));
-		Assert.assertTrue(ds.hasMoreData("ustidnrprofil"));
-		Assert.assertTrue(ds.hasMoreData("ustsv"));
-		Assert.assertTrue(ds.hasMoreData("ustva"));
-		Assert.assertTrue(ds.hasMoreData("verus"));
-		Assert.assertTrue(ds.hasMoreData("zm"));
+		Assert.assertTrue(ds.hasMoreData("countryId"));
+		Assert.assertTrue(ds.hasMoreData("languageId"));
+		Assert.assertTrue(ds.hasMoreData("searchTerm"));
 	}
 }
