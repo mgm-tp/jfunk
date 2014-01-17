6861a5133b83f3d7c65da8aad5a958146c0a8136
Reinhard Nägele <reinhard.naegele@mgm-tp.com>
2014-01-17 11:17:45 +0100

    Update license headers and NOTICE files for 2014

--- jfunk-data-generator/src/main/java/com/mgmtp/jfunk/data/generator/data/IndexedFields.java
+++ jfunk-data-generator/src/main/java/com/mgmtp/jfunk/data/generator/data/IndexedFields.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 mgm technology partners GmbH
+ * Copyright (c) 2014 mgm technology partners GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- jfunk-samples/src/main/scripts/setenv.bat
+++ jfunk-samples/src/main/scripts/setenv.bat
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright (c) 2013 mgm technology partners GmbH
+@REM Copyright (c) 2014 mgm technology partners GmbH
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
